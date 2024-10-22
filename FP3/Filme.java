0ff4bdf125aae2130f5118bf6813ce472b5caf01
Alton Jorge Vaz Freitas <8220760@estg.ipp.pt>
2024-10-22 14:59:10 +0100

    gerar codigo para cria construtor

--- FP3/Filme.java
+++ FP3/Filme.java
@@ -4,5 +4,15 @@ public class Filme{
     public Ator ator_prinicpal;
     public Ator ator_secundaria;
     public Genero genero;
+    public String dataProducao;
+
+    public Filme(int duracao_min, Ator ator_prinicpal, Ator ator_secundaria, Genero genero, String dataProducao) {
+        this.duracao_min = duracao_min;
+        this.ator_prinicpal = ator_prinicpal;
+        this.ator_secundaria = ator_secundaria;
+        this.genero = genero;
+        this.dataProducao = dataProducao;
+    }
+
 
 }
