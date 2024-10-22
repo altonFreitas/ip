374bdc94c37791652244d1eaea95ba188df56c59
Alton Jorge Vaz Freitas <8220760@estg.ipp.pt>
2024-10-22 20:57:26 +0100

    testar os inputs na classe main

--- FP3/main.java
+++ FP3/main.java
@@ -2,7 +2,20 @@ package FP3;
 
 public class main {
     public static void main(String[] args) {
-        System.out.println("Altton");
+        Ator atorPrinicpal = new Ator("ALTON FREITAS", 'M', "23-04-2003");
+        Ator atorSecundaria = new Ator("ERMELINDA TILMAN", 'F', "19-03-2006");
+        Filme filme = new Filme("SHE IS MY ONLY ONE",120, atorPrinicpal, atorSecundaria, Genero.ROMANCE, "22-10-2024");
+        System.out.println("\n**************************************************************************************************"); 
+        System.out.println("******************************* NEW MOVIE PREMIERES AT THE CINEMA ********************************");
+        System.out.println("**************************************************************************************************\n");
+        System.out.println("Title: " + filme.getTitulo());
+        System.out.println("Gender: " + filme.getGenero());
+        System.out.println("Production date: " + filme.getDataProducao());
+        System.out.println("Duration: " + filme.getDuracao_min() + " Minutes");
+        System.out.println("Actors: " + atorPrinicpal.getNome() + " and " + atorSecundaria.getNome() + "\n");
+     
+
+      
         
     }
 
