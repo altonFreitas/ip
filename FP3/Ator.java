d2c700bcda3736f3ad426e9513c6573ad1be78b7
Alton Jorge Vaz Freitas <8220760@estg.ipp.pt>
2024-10-22 20:56:39 +0100

    gerir o construtor e metodos getter para a classe ator

--- FP3/Ator.java
+++ FP3/Ator.java
@@ -1,10 +1,24 @@
 package FP3;
 
 public class Ator {
-    String nome;
-    char genero;
+    public String nome;
+    public char genero;
     public String dataNas;
 
+    public Ator (String nome, char genero, String dataNas ){
+        this.nome = nome;
+        this.genero = genero;
+        this.dataNas = dataNas;
+    }
 
-    
+    public String getNome(){
+        return nome;
+    }
+    public char getGenero(){
+        return genero;
+    }
+    public String getDataNas(){
+        return dataNas;
+    }
+  
 }
